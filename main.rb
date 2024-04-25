07b6a5655bf52281709936b6b39c0bad517e98cc
Tomas Civilis <96726777+MrDutty0@users.noreply.github.com>
2024-04-25 23:03:21 +0300

    added task schedules

--- main.rb
+++ main.rb
@@ -3,5 +3,7 @@
 require_relative 'staking_calculator'
 
 staking_calculator = StakingCalculator.create
+# Comment the line above and uncomment the line below to enable Bonus Task 2:
+# staking_calculator = StakingCalculator.create(inputting_data: true)
 
 staking_calculator.calculate_and_generate_profit_schedule
